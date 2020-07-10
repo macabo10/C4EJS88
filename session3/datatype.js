d07693148b42c68b940d44889ea976abdc32aebe
macabo10 <macabo10quymui@gmail.com>
2020-07-10 13:16:56 +0700

    Homework

---
+++ session3/datatype.js
@@ -0,0 +1,11 @@
+// let name = 'Hung'
+// let year = 2003
+// let welcome = `Hi ${name}, you're ${2020-year} years old`
+// console.log(welcome)
+// console.log('1'+3);
+
+a = Number(prompt('a='));
+b = Number(prompt('b='));
+// console.log(Number(a)+Number(b));
+console.log(`Summary: ${a} + ${b} = ${a+b}`);
+       
